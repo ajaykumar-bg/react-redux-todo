48f6b8fff187df8c0419ca279fcb2aee3d4beeed
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2019-05-10 17:20:54 +0530

    Added redux-logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import './App.css'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(logger))
 
 render(
   <Provider store={store}>
